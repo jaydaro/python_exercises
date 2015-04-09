c279ae9b2e630fbcce25b0b2d590634185ef0211
Jay Hamilton <jaydaro@yahoo.com>
2015-04-08 23:45:22 -0600

    Edited file to fix maths error

--- python_exercises/exercise3.py
+++ python_exercises/exercise3.py
@@ -31,12 +31,12 @@ def miles_per_hour():
     # barleycorn = 117.647 meters
     barleycorn_day = float((meters_per_hour * 117.648) * 24)
     # furlong = 220 yards, fortnight is 14 days
-    furlongs_fortnight = float((yards_per_hour * 220) * 336)
+    furlongs_fortnight = float((yards_per_hour / 220) * 336)
     feet_a_second = float((mph_float * 5280) / 3600)
     # mach is = 1230 feet/second
     mach_number = float(feet_a_second / 1230)
     # speed_of light = 299792458 meters/second
-    speed_of_light_percentage = float((meters_per_second / 299792458) * .1)
+    speed_of_light_percentage = float(meters_per_second / 299792458)
 
     print "Original speed in miles/hour is: %.2f" % (mph_float)
     print "Converted to barleycorn/day is: %.2f" % (barleycorn_day)
